ec083af2af609714509c63a23119f41822b9555a
cafarfan <carlos.farfan@uniminuto.edu.co>
2026-05-30 18:52:03 -0500

    Software de prueba

---
+++ desarrollo_soft_seguro-main/models/DataBase.php
@@ -0,0 +1,15 @@
+<?php
+    class DataBase{
+        #  Conexión Local
+        public static function connection(){
+            $hostname = "localhost";
+            $port = "3306";
+            $database = "db_inventory";
+            $username = "root";
+            $password = "";
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $pdo;
+		}
+	}
+?>
